7879e5654779e1c70422105b3d5f25c98b62f615
AMS <alberto@oscuro.es>
2020-02-14 17:03:17 +0100

    Adapt portfolio deployment script for Lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,27 @@
-
 import boto3
 import io
 import zipfile
 import mimetypes
+import json
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('portfolio.ktulu.eu')
-build_bucket = s3.Bucket('portfolio.ktulu.eu-build')
+    s3 = boto3.resource('s3')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
+    portfolio_bucket = s3.Bucket('portfolio.ktulu.eu')
+    build_bucket = s3.Bucket('portfolio.ktulu.eu-build')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        print('Extract and upload file:' + nm)
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            print('Extract and upload file:' + nm)
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Files uploaded to S3')
+    }
