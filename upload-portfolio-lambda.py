e2f9803730b677b3da12886ef5d9e4ad2b23d8ad
AMS <alberto@oscuro.es>
2020-02-14 16:32:19 +0100

    Add upload portfolio script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,20 @@
+
+import boto3
+import io
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.ktulu.eu')
+build_bucket = s3.Bucket('portfolio.ktulu.eu-build')
+
+portfolio_zip = io.BytesIO()
+build_bucket.download_fileobj('portfolio-build.zip', portfolio_zip)
+
+with zipfile.ZipFile('/tmp/portfolio-build.zip') as myzip:
+    for nm in myzip.namelist():
+        print(nm)
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
